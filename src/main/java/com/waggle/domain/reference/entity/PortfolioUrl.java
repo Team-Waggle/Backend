236487966f32954f18d150176b164f1b34101fab
KIMB0B <axzsd96301@gmail.com>
2025-01-09 23:19:27 +0900

    fix: 포트폴리오 URL 종류 엔티티에 로고 이미지 URL 필드 추가

--- src/main/java/com/waggle/domain/reference/entity/PortfolioUrl.java
+++ src/main/java/com/waggle/domain/reference/entity/PortfolioUrl.java
@@ -20,6 +20,9 @@ public class PortfolioUrl {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "img_url")
+    private String imgUrl;
+
     @Column(name = "name", nullable = false)
     private String name;
 
