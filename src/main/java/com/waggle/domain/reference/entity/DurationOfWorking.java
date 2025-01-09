9ed83da99f4a95ec9b93958d020ad1594e0b9143
KIMB0B <axzsd96301@gmail.com>
2025-01-09 23:16:31 +0900

    feat: 참조 데이터 엔티티 생성

---
+++ src/main/java/com/waggle/domain/reference/entity/DurationOfWorking.java
@@ -0,0 +1,29 @@
+package com.waggle.domain.reference.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "dow_type")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class DurationOfWorking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
