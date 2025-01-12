5401e541eafa681d1c2b3186e9c73af8bc720705
ssarisong <thdtnwls2989@naver.com>
2025-01-12 15:51:27 +0900

    feat: 프로젝트 CRUD 서비스 추가

--- src/main/java/com/waggle/domain/project/service/ProjectService.java
+++ src/main/java/com/waggle/domain/project/service/ProjectService.java
@@ -1,8 +1,14 @@
 package com.waggle.domain.project.service;
 
 import com.waggle.domain.project.dto.CreateProjectDto;
+import com.waggle.domain.project.dto.UpdateProjectDto;
 import com.waggle.domain.project.entity.Project;
+
+import java.util.UUID;
 
 public interface ProjectService {
     Project create(CreateProjectDto createProjectDto);
+    Project update(UUID id, UpdateProjectDto updateProjectDto);
+    void delete(UUID id);
+    Project findById(UUID id);
 }
