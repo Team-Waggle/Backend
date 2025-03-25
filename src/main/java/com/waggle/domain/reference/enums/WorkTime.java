c886d32c61a75db6ac067c62e7014c32f1c313b1
sillysillman <sillysillyman.cs@gmail.com>
2025-03-26 02:59:39 +0900

    feat: 작업 시간 enum 클래스 생성

---
+++ src/main/java/com/waggle/domain/reference/enums/WorkTime.java
@@ -0,0 +1,16 @@
+package com.waggle.domain.reference.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum WorkTime {
+    MORNING("오전"),
+    AFTERNOON("오후"),
+    EVENING("저녁"),
+    NIGHT("심야"),
+    DAWN("새벽");
+
+    private final String name;
+}
