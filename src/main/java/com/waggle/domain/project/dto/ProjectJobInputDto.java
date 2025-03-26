72963ad3791209bffc0af60133f914d137747b55
sillysillman <sillysillyman.cs@gmail.com>
2025-03-27 02:13:57 +0900

    refactor: 프로젝트 담당 업무 요청 DTO record 사용하도록 변경

--- src/main/java/com/waggle/domain/project/dto/ProjectJobInputDto.java
+++ src/main/java/com/waggle/domain/project/dto/ProjectJobInputDto.java
@@ -2,16 +2,16 @@ package com.waggle.domain.project.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.*;
 
-@Getter
-public class ProjectJobInputDto {
+public record ProjectJobInputDto(
 
     @JsonProperty("job_id")
     @Schema(description = "직무 고유키", example = "1")
-    private Long jobId;
+    Long jobId,
 
     @JsonProperty("cnt")
     @Schema(description = "인원", example = "3")
-    private int cnt;
+    int cnt
+) {
+
 }
