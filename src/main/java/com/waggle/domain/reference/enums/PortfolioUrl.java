60911aefa93ab2c574030639c414a390e080898c
sillysillyman <sillysillyman.cs@gmail.com>
2025-03-27 04:43:03 +0900

    feat: 포트폴리오 URL enum 클래스 생성

---
+++ src/main/java/com/waggle/domain/reference/enums/PortfolioUrl.java
@@ -0,0 +1,23 @@
+package com.waggle.domain.reference.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum PortfolioUrl {
+    GITHUB("GitHub", "https://logo.clearbit.com/GitHub.com"),
+    NOTION("Notion", "https://logo.clearbit.com/Notion.com"),
+    LINKEDIN("LinkedIn", "https://logo.clearbit.com/LinkedIn.com"),
+    YOUTUBE("YouTube", "https://logo.clearbit.com/YouTube.com"),
+    INSTAGRAM("Instagram", "https://logo.clearbit.com/Instagram.com"),
+    BRUNCH("Brunch", "https://logo.clearbit.com/Brunch.co.kr"),
+    TWITTER("Twitter", "https://logo.clearbit.com/Twitter.com"),
+    DRIBBBLE("Dribbble", "https://logo.clearbit.com/Dribbble.com"),
+    TRELLO("Trello", "https://logo.clearbit.com/Trello.com"),
+    FIGMA("Figma", "https://logo.clearbit.com/Figma.com"),
+    OTHER("기타", "");
+
+    private final String name;
+    private final String imageUrl;
+}
