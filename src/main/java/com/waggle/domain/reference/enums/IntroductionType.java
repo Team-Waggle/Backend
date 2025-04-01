177f95b2046c0c1cb7369db377b06e1ed1bcad3e
sillysillman <sillysillyman.cs@gmail.com>
2025-04-01 16:19:04 +0900

    feat: 소개 타입 enum 클래스 생성

---
+++ src/main/java/com/waggle/domain/reference/enums/IntroductionType.java
@@ -0,0 +1,18 @@
+package com.waggle.domain.reference.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum IntroductionType {
+    COMMUNICATION_STYLE("소통 스타일", CommunicationStyle.class),
+    COLLABORATION_STYLE("협업 성향", CollaborationStyle.class),
+    WORK_TIME("작업 시간대", WorkTime.class),
+    WORK_WAY("작업 방식", WorkStyle.class),
+    PROBLEM_SOLVING_APPROACH("문제 해결 방식", ProblemSolvingApproach.class),
+    MBTI("MBTI", Mbti.class);
+
+    private final String description;
+    private final Class<? extends Enum<?>> enumClass;
+}
