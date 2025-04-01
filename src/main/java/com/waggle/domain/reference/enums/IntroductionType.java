a0743a8dc1f21a53a8d21b448139222c52c392cd
sillysillman <sillysillyman.cs@gmail.com>
2025-04-02 05:14:07 +0900

    fix: 잘못된 값 수정

--- src/main/java/com/waggle/domain/reference/enums/IntroductionType.java
+++ src/main/java/com/waggle/domain/reference/enums/IntroductionType.java
@@ -8,8 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum IntroductionType {
     COMMUNICATION_STYLE("소통 스타일", CommunicationStyle.class),
     COLLABORATION_STYLE("협업 성향", CollaborationStyle.class),
-    WORK_TIME("작업 시간대", WorkTime.class),
-    WORK_WAY("작업 방식", WorkStyle.class),
+    WORK_STYLE("작업 방식", WorkStyle.class),
     PROBLEM_SOLVING_APPROACH("문제 해결 방식", ProblemSolvingApproach.class),
     MBTI("MBTI", Mbti.class);
 
