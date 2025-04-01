4972280fcf7808d429752bed7c37a4664d1ee504
sillysillman <sillysillyman.cs@gmail.com>
2025-04-02 05:20:37 +0900

    refactor: 세터 제거 및 사용자 관련 양방향 관계 제거
    - updateProfile 메서드로 정보 수정

--- src/main/java/com/waggle/domain/user/entity/User.java
+++ src/main/java/com/waggle/domain/user/entity/User.java
@@ -1,36 +1,26 @@
 package com.waggle.domain.user.entity;
 
-import com.waggle.domain.project.entity.ProjectApplicant;
-import com.waggle.domain.project.entity.ProjectBookmark;
-import com.waggle.domain.project.entity.ProjectMember;
 import com.waggle.domain.reference.enums.Sido;
 import com.waggle.domain.reference.enums.WorkTime;
 import com.waggle.domain.reference.enums.WorkWay;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
 @Getter
-@Setter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
@@ -57,44 +47,26 @@ public class User {
     @Column(name = "email", nullable = false)
     private String email;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
-    private Set<UserJobRole> userJobRoles = new HashSet<>();
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
-    private Set<UserIndustry> userIndustries = new HashSet<>();
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
-    private Set<UserSkill> userSkills = new HashSet<>();
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
-    private Set<UserDayOfWeek> userDayOfWeeks = new HashSet<>();
-
     @Column(name = "preferred_work_time")
-    private WorkTime preferredWorkTime;
+    private WorkTime workTime;
 
     @Column(name = "preferred_work_way")
-    private WorkWay preferredWorkWay;
+    private WorkWay workWay;
 
     @Column(name = "preferred_sido")
-    private Sido preferredSido;
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
-    private Set<UserIntroduction> userIntroductions = new HashSet<>();
+    private Sido sido;
 
     @Column(name = "detail")
     private String detail;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
-    private Set<UserPortfolio> userPortfolios = new HashSet<>();
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
-    private Set<ProjectMember> projectMembers = new HashSet<>();
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
-    private Set<ProjectApplicant> projectApplicants = new HashSet<>();
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
-    private Set<ProjectBookmark> projectBookmarks = new HashSet<>();
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+//    private Set<ProjectMember> projectMembers = new HashSet<>();
+//
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+//    private Set<ProjectApplicant> projectApplicants = new HashSet<>();
+//
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+//    private Set<ProjectBookmark> projectBookmarks = new HashSet<>();
 
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
@@ -103,4 +75,20 @@ public class User {
     @UpdateTimestamp
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
+
+    public void updateProfile(
+        String name,
+        String profileImageUrl,
+        WorkTime workTime,
+        WorkWay workWay,
+        Sido sido,
+        String detail
+    ) {
+        this.name = name;
+        this.profileImageUrl = profileImageUrl;
+        this.workTime = workTime;
+        this.workWay = workWay;
+        this.sido = sido;
+        this.detail = detail;
+    }
 }
