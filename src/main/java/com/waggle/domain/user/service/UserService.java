6d3abaa86088c978fdd51549f596412c1fea0250
sillysillman <sillysillyman.cs@gmail.com>
2025-04-01 16:29:48 +0900

    chore: 린팅

--- src/main/java/com/waggle/domain/user/service/UserService.java
+++ src/main/java/com/waggle/domain/user/service/UserService.java
@@ -1,15 +1,16 @@
 package com.waggle.domain.user.service;
 
-import com.waggle.domain.project.entity.Project;
 import com.waggle.domain.user.dto.UserInputDto;
 import com.waggle.domain.user.entity.User;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Set;
-
 public interface UserService {
+
     User getCurrentUser();
+
     User updateCurrentUser(MultipartFile profileImage, UserInputDto userInputDto);
+
     void deleteCurrentUser();
+
     User getUserByUserId(String userId);
 }
