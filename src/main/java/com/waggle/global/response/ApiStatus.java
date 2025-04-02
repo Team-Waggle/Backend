c2bacc67ad8e04821fc12d891811562606968250
sillysillman <sillysillyman.cs@gmail.com>
2025-04-03 00:27:22 +0900

    chore: 린팅 및 주석 기준 행 분리

--- src/main/java/com/waggle/global/response/ApiStatus.java
+++ src/main/java/com/waggle/global/response/ApiStatus.java
@@ -12,7 +12,8 @@ public enum ApiStatus {
     _CREATED(HttpStatus.CREATED, 201, "생성에 성공했습니다."),
     _ACCEPTED(HttpStatus.ACCEPTED, 202, "요청이 수락되었습니다."),
     _NO_CONTENT(HttpStatus.NO_CONTENT, 204, "No Content"),
-        // 커스텀
+
+    // 커스텀
     _REISSUE_ACCESS_TOKEN(HttpStatus.CREATED, 201, "액세스 토큰 재발행에 성공했습니다."),
     _CREATE_ACCESS_TOKEN(HttpStatus.CREATED, 201, "액세스 토큰 발행에 성공했습니다."),
 
@@ -25,12 +26,15 @@ public enum ApiStatus {
     _CONFLICT(HttpStatus.CONFLICT, 409, "충돌이 발생했습니다."),
     _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500, "서버 내부 오류가 발생했습니다."),
     _SERVICE_UNAVAILABLE(HttpStatus.SERVICE_UNAVAILABLE, 503, "서비스를 사용할 수 없습니다."),
-        // 커스텀
+
+    // 커스텀
     _INVALID_TOKEN(HttpStatus.UNAUTHORIZED, 401, "유효하지 않은 토큰입니다."),
+    _EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, 401, "토큰이 만료되었습니다."),
     _INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, 401, "유효하지 않은 액세스 토큰입니다."),
     _INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, 401, "유효하지 않은 리프레쉬 토큰입니다."),
     _INVALID_TEMPORARY_TOKEN(HttpStatus.UNAUTHORIZED, 401, "유효하지 않은 임시 토큰입니다."),
     _REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, 404, "해당 유저 ID의 리프레쉬 토큰이 없습니다."),
+
     // S3 관련 에러 (4xx)
     _S3_INVALID_URL(HttpStatus.BAD_REQUEST, 400, "유효하지 않은 URL입니다."),
     _S3_UPLOAD_FAILED(HttpStatus.BAD_REQUEST, 400, "파일 업로드에 실패했습니다."),
