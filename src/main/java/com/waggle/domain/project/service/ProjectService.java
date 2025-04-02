a2900c1dea40b0b3893d06afea9bbd98839dbaa1
sillysillman <sillysillyman.cs@gmail.com>
2025-04-03 00:43:48 +0900

    chore: 린팅

--- src/main/java/com/waggle/domain/project/service/ProjectService.java
+++ src/main/java/com/waggle/domain/project/service/ProjectService.java
@@ -3,28 +3,46 @@ package com.waggle.domain.project.service;
 import com.waggle.domain.project.dto.ProjectInputDto;
 import com.waggle.domain.project.entity.Project;
 import com.waggle.domain.user.entity.User;
-
 import java.util.Set;
 import java.util.UUID;
 
 public interface ProjectService {
+
     Project getProjectByProjectId(UUID id);
+
     Project createProject(ProjectInputDto projectInputDto);
+
     Project updateProject(UUID id, ProjectInputDto projectInputDto);
+
     void deleteProject(UUID id);
+
     Set<User> getUsersByProjectId(UUID id);
+
     Set<User> getAppliedUsersByProjectId(UUID id);
+
     Set<User> approveAppliedUser(UUID projectId, String userId);
+
     Set<User> rejectAppliedUser(UUID projectId, String userId);
+
     Set<User> rejectMemberUser(UUID projectId, String userId);
+
     void delegateLeader(UUID projectId, String userId);
+
     Set<Project> getUserProjects(String userId);
+
     void deleteUserProject(String projectId);
+
     Set<Project> getUserBookmarkProjects(String userId);
+
     Project applyProject(String projectId);
+
     void cancelApplyProject(String projectId);
+
     Set<Project> getAppliedProjects();
+
     boolean toggleCurrentUserBookmark(String projectId);
+
     Set<Project> getCurrentUserBookmarkProjects();
+
     Set<Project> getCurrentUserProjects();
 }
