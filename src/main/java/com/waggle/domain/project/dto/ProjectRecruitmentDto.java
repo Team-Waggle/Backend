fd764cfe6e6c9ff43344ff381b34ec45121f4e69
sillysillman <sillysillyman.cs@gmail.com>
2025-04-07 23:47:49 +0900

    feat: 프로젝트 모집 DTO 추가

---
+++ src/main/java/com/waggle/domain/project/dto/ProjectRecruitmentDto.java
@@ -0,0 +1,34 @@
+package com.waggle.domain.project.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.waggle.domain.project.entity.ProjectRecruitment;
+import com.waggle.domain.reference.enums.JobRole;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record ProjectRecruitmentDto(
+    @Schema(description = "직무", example = "JAVA")
+    @JsonProperty("job_role")
+    JobRole jobRole,
+
+    @Schema(description = "모집 인원 수", example = "2")
+    @JsonProperty("remaining_count")
+    int remainingCount,
+
+    @Schema(description = "현재 인원 수", example = "3")
+    @JsonProperty("current_count")
+    int currentCount,
+
+    @Schema(description = "필요 인원 수", example = "5")
+    @JsonProperty("required_count")
+    int requiredCount
+) {
+
+    public static ProjectRecruitmentDto from(ProjectRecruitment recruitment) {
+        return new ProjectRecruitmentDto(
+            recruitment.getJobRole(),
+            recruitment.getRequiredCount() - recruitment.getCurrentCount(),
+            recruitment.getCurrentCount(),
+            recruitment.getRequiredCount()
+        );
+    }
+}
