e3d2a73d1531e2bf1a7d30d3f2f26cb7938fbacb
sillysillman <sillysillyman.cs@gmail.com>
2025-04-09 03:18:59 +0900

    feat: 프로젝트 ID 기반 목록 조회 JPA 메서드 추가

--- src/main/java/com/waggle/domain/project/repository/ProjectApplicantRepository.java
+++ src/main/java/com/waggle/domain/project/repository/ProjectApplicantRepository.java
@@ -1,10 +1,13 @@
 package com.waggle.domain.project.repository;
 
 import com.waggle.domain.project.entity.ProjectApplicant;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectApplicantRepository extends JpaRepository<ProjectApplicant, UUID> {
+
+    List<ProjectApplicant> findByProjectId(UUID projectId);
 
     void deleteByProjectId(UUID projectId);
 }
--- src/main/java/com/waggle/domain/project/repository/ProjectSkillRepository.java
+++ src/main/java/com/waggle/domain/project/repository/ProjectSkillRepository.java
@@ -1,10 +1,13 @@
 package com.waggle.domain.project.repository;
 
 import com.waggle.domain.project.entity.ProjectSkill;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectSkillRepository extends JpaRepository<ProjectSkill, UUID> {
+
+    List<ProjectSkill> findByProjectId(UUID projectId);
 
     void deleteByProjectId(UUID projectId);
 }
