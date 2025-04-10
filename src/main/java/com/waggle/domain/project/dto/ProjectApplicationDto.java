b3bb3fc040e6f3e8c5e3dd24f11e3d285c2619f9
sillysillman <sillysillyman.cs@gmail.com>
2025-04-10 18:22:06 +0900

    feat: 지원 요청 시 직무 정보 DTO 추가

---
+++ src/main/java/com/waggle/domain/project/dto/ProjectApplicationDto.java
@@ -0,0 +1,15 @@
+package com.waggle.domain.project.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.waggle.domain.reference.enums.JobRole;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+
+public record ProjectApplicationDto(
+    @NotNull(message = "지원 직무는 필수 입력 항목입니다")
+    @Schema(description = "지원 직무", example = "BACKEND")
+    @JsonProperty("job_role")
+    JobRole jobRole
+) {
+
+}
