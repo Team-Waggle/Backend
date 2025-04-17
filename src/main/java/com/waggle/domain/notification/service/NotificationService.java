6289d38afee09556f1ba583856e109071414c9de
sillysillman <sillysillyman.cs@gmail.com>
2025-04-18 01:19:20 +0900

    feat: 알림 서비스 추가

---
+++ src/main/java/com/waggle/domain/notification/service/NotificationService.java
@@ -0,0 +1,5 @@
+package com.waggle.domain.notification.service;
+
+public interface NotificationService {
+
+}
---
+++ src/main/java/com/waggle/domain/notification/service/NotificationServiceImpl.java
@@ -0,0 +1,12 @@
+package com.waggle.domain.notification.service;
+
+import com.waggle.domain.notification.repository.NotificationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationServiceImpl implements NotificationService {
+
+    private final NotificationRepository notificationRepository;
+}
