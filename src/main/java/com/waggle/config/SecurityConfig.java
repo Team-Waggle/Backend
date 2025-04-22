8552d3af341554e17cc42f6c5e786068c8b9aa59
sillysillman <sillysillyman.cs@gmail.com>
2025-04-23 03:26:18 +0900

    feat: 인증 필터 및 OAuth2 서비스 추가

--- src/main/java/com/waggle/config/SecurityConfig.java
+++ src/main/java/com/waggle/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.waggle.config;
 import com.waggle.global.secure.filter.JwtAuthenticationFilter;
 import com.waggle.global.secure.oauth2.handler.OAuth2LoginFailureHandler;
 import com.waggle.global.secure.oauth2.handler.OAuth2LoginSuccessHandler;
+import com.waggle.global.secure.oauth2.service.CustomOAuth2UserService;
 import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -24,6 +25,7 @@ public class SecurityConfig {
     private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
     private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     CorsConfigurationSource corsConfigurationSource() {
         return request -> {
@@ -44,14 +46,12 @@ public class SecurityConfig {
                 corsConfigurationSource())) // CORS 설정 추가
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(authorize ->
-                authorize
-                    .requestMatchers("/api/v1/auth/**").permitAll()
-                    .requestMatchers("/api/v1/users/**").authenticated()
-                    .anyRequest().permitAll()
+                authorize.requestMatchers("/**").permitAll()
             )
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
             .oauth2Login(oauth -> // OAuth2 로그인 기능에 대한 여러 설정의 진입점
                 oauth
+                    .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
                     .successHandler(oAuth2LoginSuccessHandler) // 로그인 성공 시 핸들러
                     .failureHandler(oAuth2LoginFailureHandler) // 로그인 실패 시 핸들러
             )
