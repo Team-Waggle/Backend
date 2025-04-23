beead3a3adf6fc0e6b65528ceee137eab3a735b0
sillysillman <sillysillyman.cs@gmail.com>
2025-04-23 19:28:37 +0900

    feat: JPA 메서드 추가

--- src/main/java/com/waggle/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/waggle/domain/notification/repository/NotificationRepository.java
@@ -1,9 +1,17 @@
 package com.waggle.domain.notification.repository;
 
 import com.waggle.domain.notification.entity.Notification;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
 
+    int countByUserIdAndIsReadFalse(UUID userId);
+
+    List<Notification> findByUserIdOrderByCreatedAtDesc(UUID userId);
+
+    @Modifying
+    int updateIsReadByUserIdAndIsReadFalse(UUID userId, boolean isRead);
 }
