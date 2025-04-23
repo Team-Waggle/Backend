7ed9efde68173b5f7ed424c2637aca524d4821f0
sillysillman <sillysillyman.cs@gmail.com>
2025-04-24 01:11:25 +0900

    feat: JPQL 업데이트 쿼리 작성

--- src/main/java/com/waggle/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/waggle/domain/notification/repository/NotificationRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
 
@@ -13,5 +14,6 @@ public interface NotificationRepository extends JpaRepository<Notification, UUID
     List<Notification> findByUserIdOrderByCreatedAtDesc(UUID userId);
 
     @Modifying
+    @Query("UPDATE Notification n SET n.isRead = :isRead WHERE n.user.id = :userId AND n.isRead = false")
     int updateIsReadByUserIdAndIsReadFalse(UUID userId, boolean isRead);
 }
