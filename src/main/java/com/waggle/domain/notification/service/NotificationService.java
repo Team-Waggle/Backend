384009b368e84d66862ec9e4ab9b88ca9df47a57
sillysillman <sillysillyman.cs@gmail.com>
2025-04-23 19:27:57 +0900

    feat: 알림 서비스 메서드 시그니처 정의

--- src/main/java/com/waggle/domain/notification/service/NotificationService.java
+++ src/main/java/com/waggle/domain/notification/service/NotificationService.java
@@ -1,5 +1,14 @@
 package com.waggle.domain.notification.service;
 
+import com.waggle.domain.notification.entity.Notification;
+import com.waggle.domain.user.entity.User;
+import java.util.Set;
+
 public interface NotificationService {
 
+    Set<Notification> getNotifications(User user);
+
+    int getUnreadNotificationCount(User user);
+
+    void readAllNotifications(User user);
 }
