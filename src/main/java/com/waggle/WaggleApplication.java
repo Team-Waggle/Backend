66bfdd041bad2dee0b8e02d3abe4c6b94539d764
sillysillman <sillysillyman.cs@gmail.com>
2025-05-17 18:40:13 +0900

    feat: 스케줄링 애너테이션 추가

--- src/main/java/com/waggle/WaggleApplication.java
+++ src/main/java/com/waggle/WaggleApplication.java
@@ -2,12 +2,14 @@ package com.waggle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class WaggleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WaggleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WaggleApplication.class, args);
+    }
 
 }
