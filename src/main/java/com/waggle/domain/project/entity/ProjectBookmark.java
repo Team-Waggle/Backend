1cf66717c29fb5e45e6fbb612c5e90c0cd1933db
sillysillman <sillysillyman.cs@gmail.com>
2025-06-06 01:19:16 +0900

    fix: json 애너테이션 제거

--- src/main/java/com/waggle/domain/project/entity/ProjectBookmark.java
+++ src/main/java/com/waggle/domain/project/entity/ProjectBookmark.java
@@ -1,15 +1,20 @@
 package com.waggle.domain.project.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.waggle.domain.user.entity.User;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
-
-import java.util.UUID;
 
 @Entity
 @Getter
@@ -20,20 +25,15 @@ public class ProjectBookmark {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @JsonIgnore
     private UUID id;
 
     @ManyToOne
     @JoinColumn(name = "project_id")
-    @JsonProperty("project")
-    @JsonIgnoreProperties("users")
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Project project;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
-    @JsonProperty("user")
-    @JsonIgnoreProperties("projects")
     @OnDelete(action = OnDeleteAction.CASCADE)
     private User user;
 }
