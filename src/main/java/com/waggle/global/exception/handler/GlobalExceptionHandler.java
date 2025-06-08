929c03ffe29cd2be4932ffbed8d842eca305ec5c
sillysillman <sillysillyman.cs@gmail.com>
2025-06-08 20:30:20 +0900

    fix: 반환 타입 변경

--- src/main/java/com/waggle/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/waggle/global/exception/handler/GlobalExceptionHandler.java
@@ -7,7 +7,6 @@ import com.waggle.global.exception.S3Exception;
 import com.waggle.global.response.ApiStatus;
 import com.waggle.global.response.BaseResponse;
 import com.waggle.global.response.ErrorResponse;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -18,44 +17,38 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(JwtTokenException.class)
-    public ResponseEntity<BaseResponse<String>> handleJwtTokenException(JwtTokenException ex) {
+    public ResponseEntity<BaseResponse<Void>> handleJwtTokenException(JwtTokenException ex) {
         return ErrorResponse.of(ex.getStatus());
     }
 
-    @ExceptionHandler(EmptyResultDataAccessException.class)
-    public ResponseEntity<BaseResponse<String>> handleEmptyResultDataAccessException(
-        EmptyResultDataAccessException ex) {
-        return ErrorResponse.of(ApiStatus._NOT_FOUND);
-    }
-
     @ExceptionHandler(S3Exception.class)
-    public ResponseEntity<BaseResponse<Object>> handleS3Exception(S3Exception ex) {
+    public ResponseEntity<BaseResponse<Void>> handleS3Exception(S3Exception ex) {
         return ErrorResponse.of(ex.getStatus(), ex.getMessage());
     }
 
     @ExceptionHandler(AccessDeniedException.class)
-    public ResponseEntity<BaseResponse<Object>> handleAccessDeniedException(
-        AccessDeniedException ex) {
+    public ResponseEntity<BaseResponse<Void>> handleAccessDeniedException(
+        AccessDeniedException ex
+    ) {
         return ErrorResponse.of(ex.getStatus());
     }
 
     @ExceptionHandler(ProjectException.class)
-    public ResponseEntity<BaseResponse<Object>> handleProjectException(ProjectException ex) {
+    public ResponseEntity<BaseResponse<Void>> handleProjectException(ProjectException ex) {
         return ErrorResponse.of(ex.getStatus(), ex.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<BaseResponse<Object>> handleIllegalArgumentException(
+    public ResponseEntity<BaseResponse<Void>> handleIllegalArgumentException(
         IllegalArgumentException ex
     ) {
         return ErrorResponse.of(ApiStatus._BAD_REQUEST, ex.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<BaseResponse<Object>> handleMethodArgumentNotValidException(
+    public ResponseEntity<BaseResponse<Void>> handleMethodArgumentNotValidException(
         MethodArgumentNotValidException ex
     ) {
-
         StringBuilder errorMessage = new StringBuilder();
 
         ex.getBindingResult().getAllErrors().forEach((error) -> {
