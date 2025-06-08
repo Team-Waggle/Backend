48ed5664ddeca713d96e4091b2491648a58fa444
sillysillman <sillysillyman.cs@gmail.com>
2025-06-08 19:01:31 +0900

    fix: 중복 필드 제거

--- src/main/java/com/waggle/global/response/SuccessResponse.java
+++ src/main/java/com/waggle/global/response/SuccessResponse.java
@@ -6,23 +6,8 @@ import org.springframework.http.ResponseEntity;
 @Schema(description = "성공 응답 엔티티")
 public class SuccessResponse<T> extends BaseResponse<T> {
 
-    @Schema(description = "성공 여부", example = "true")
-    protected final boolean isSuccess = true;
-
-    @Schema(description = "응답 코드", example = "200")
-    protected final int code;
-
-    @Schema(description = "응답 메시지", example = "성공적으로 처리되었습니다.")
-    protected final String message;
-
-    @Schema(description = "응답 데이터")
-    protected final T payload;
-
     public SuccessResponse(int code, String message, T payload) {
         super(true, code, message, payload);
-        this.code = code;
-        this.message = message;
-        this.payload = payload;
     }
 
     public static <T> ResponseEntity<BaseResponse<T>> of(ApiStatus code, T data) {
