407b43c2e03a662c532f87b39707109756eafba1
sillysillman <sillysillyman.cs@gmail.com>
2025-06-08 22:32:42 +0900

    refactor: UTC 사용하도록 변경

--- src/main/java/com/waggle/global/entity/BaseEntity.java
+++ src/main/java/com/waggle/global/entity/BaseEntity.java
@@ -3,7 +3,7 @@ package com.waggle.global.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
+import java.time.Instant;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -16,9 +16,9 @@ public abstract class BaseEntity {
 
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    private Instant createdAt;
 
     @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
-    private LocalDateTime updatedAt;
+    private Instant updatedAt;
 }
