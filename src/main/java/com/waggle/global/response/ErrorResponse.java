42b4e388058399a74113d24c887840308e4c14da
sillysillman <sillysillyman.cs@gmail.com>
2025-06-08 21:51:34 +0900

    fix: 불필요한 제네릭 삭제

--- src/main/java/com/waggle/global/response/ErrorResponse.java
+++ src/main/java/com/waggle/global/response/ErrorResponse.java
@@ -4,19 +4,19 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.http.ResponseEntity;
 
 @Schema(description = "에러 응답 엔티티")
-public class ErrorResponse<T> extends BaseResponse<Void> {
+public class ErrorResponse extends BaseResponse<Void> {
 
     public ErrorResponse(int code, String message) {
         super(false, code, message, null);
     }
 
     public static ResponseEntity<BaseResponse<Void>> of(ApiStatus code) {
-        BaseResponse<Void> response = new ErrorResponse<>(code.getCode(), code.getMessage());
+        BaseResponse<Void> response = new ErrorResponse(code.getCode(), code.getMessage());
         return ResponseEntity.status(code.getHttpStatus()).body(response);
     }
 
     public static ResponseEntity<BaseResponse<Void>> of(ApiStatus code, String message) {
-        BaseResponse<Void> response = new ErrorResponse<>(code.getCode(), message);
+        BaseResponse<Void> response = new ErrorResponse(code.getCode(), message);
         return ResponseEntity.status(code.getHttpStatus()).body(response);
     }
 }
