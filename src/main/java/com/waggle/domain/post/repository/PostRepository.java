b975b3353a8deb7e0fd2caf988328927a7a321fd
sillysillman <sillysillyman.cs@gmail.com>
2025-06-08 23:46:41 +0900

    feat: 정렬 조건 추가

--- src/main/java/com/waggle/domain/post/repository/PostRepository.java
+++ src/main/java/com/waggle/domain/post/repository/PostRepository.java
@@ -9,9 +9,22 @@ import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    @Query("SELECT p FROM Post p JOIN FETCH p.user LEFT JOIN FETCH p.project WHERE p.id = :id")
+    @Query("""
+        SELECT p
+        FROM Post p
+        JOIN FETCH p.user
+        LEFT JOIN FETCH p.project
+        WHERE p.id = :id
+        ORDER BY p.createdAt DESC, p.id DESC
+        """)
     Optional<Post> findByIdWithRelations(@Param("id") Long postId);
 
-    @Query("SELECT p FROM Post p JOIN FETCH p.user LEFT JOIN FETCH p.project")
+    @Query("""
+        SELECT p
+        FROM Post p
+        JOIN FETCH p.user
+        LEFT JOIN FETCH p.project
+        ORDER BY p.createdAt DESC, p.id DESC
+        """)
     List<Post> findAllWithRelations();
 }
