92503a6fa557aa826a686c82ae3bd66b03b24ba1
sillysillman <sillysillyman.cs@gmail.com>
2025-06-09 00:42:41 +0900

    fix: 타입 변경

--- src/main/java/com/waggle/domain/projectV2/repository/ProjectV2Repository.java
+++ src/main/java/com/waggle/domain/projectV2/repository/ProjectV2Repository.java
@@ -1,8 +1,9 @@
 package com.waggle.domain.projectV2.repository;
 
 import com.waggle.domain.projectV2.ProjectV2;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProjectV2Repository extends JpaRepository<ProjectV2, Long> {
+public interface ProjectV2Repository extends JpaRepository<ProjectV2, UUID> {
 
 }
