b60be5e8bacb14448ca2dd5b82d305854f346294
sillysillman <sillysillyman.cs@gmail.com>
2025-08-06 22:32:27 +0900

    feat: 작업 기간 enum 변경

--- src/main/java/com/waggle/domain/reference/enums/WorkPeriod.java
+++ src/main/java/com/waggle/domain/reference/enums/WorkPeriod.java
@@ -9,13 +9,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum WorkPeriod {
-    ONE_MONTH("1개월"),
-    TWO_MONTHS("2개월"),
-    THREE_MONTHS("3개월"),
-    FOUR_MONTHS("4개월"),
-    FIVE_MONTHS("5개월"),
-    SIX_MONTHS("6개월"),
-    OVER_SIX_MONTHS("6개월 이상");
+    SHORT_TERM("단기"),
+    MEDIUM_TERM("중기"),
+    LONG_TERM("장기"),
+    UNDECIDED("미정");
 
     @JsonProperty("display_name")
     private final String displayName;
