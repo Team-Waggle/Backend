a0a9742a79355b84bffdb153c3046df0937ba954
sillysillman <sillysillyman.cs@gmail.com>
2025-09-04 20:58:04 +0900

    fix: JsonFormat 제거

--- src/main/java/com/waggle/domain/reference/enums/CollaborationStyle.java
+++ src/main/java/com/waggle/domain/reference/enums/CollaborationStyle.java
@@ -1,12 +1,5 @@
 package com.waggle.domain.reference.enums;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
-@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum CollaborationStyle {
     LEADERSHIP,
     SUPPORTER,
