3a9f7450635b11b1a500fe592c3348181cf551d6
sillysillman <sillysillyman.cs@gmail.com>
2025-10-29 22:28:51 +0900

    feat: 불필요한 DTO 제거

--- src/main/java/com/waggle/domain/project/dto/ProjectConfirmApplicationDto.java
+++
@@ -1,8 +0,0 @@
-package com.waggle.domain.project.dto;
-
-import jakarta.validation.constraints.NotNull;
-import java.util.UUID;
-
-public record ProjectConfirmApplicationDto(@NotNull UUID projectApplicantId) {
-
-}
